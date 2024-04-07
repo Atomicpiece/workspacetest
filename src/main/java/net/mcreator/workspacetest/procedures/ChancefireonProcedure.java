60b1ffa75068fbf4bf11bcbf358244b1e99c6794
Atomicpiece <tomas@pc-tomas>
2024-04-07 17:42:06 +0200

    Biomes deleted

--- src/main/java/net/mcreator/workspacetest/procedures/ChancefireonProcedure.java
+++ src/main/java/net/mcreator/workspacetest/procedures/ChancefireonProcedure.java
@@ -1,6 +1,15 @@
 package net.mcreator.workspacetest.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.core.BlockPos;
 
 import javax.annotation.Nullable;
 
